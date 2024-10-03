7c177249683191ddbabdd85c1b02cf50328859c6
Emanuel Ghetiu <emanuel@optiononetech.com>
2024-10-03 13:16:26 +0300

    Modificari cod Department and Level

--- src/OptionOneTech.AlertSystem.Domain/Levels/Level.cs
+++ src/OptionOneTech.AlertSystem.Domain/Levels/Level.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,12 @@ namespace OptionOneTech.AlertSystem.Levels
 {
     public class Level : FullAuditedEntity<Guid>
     {
+        [Required]
         public string Name { get; set; }
+
+        [MaxLength(512)]
         public string Description { get; set; }
+
         public bool Active { get; set; }
     }
 }
