da440fb03512c96a03df2458d13e8b4a07cf7209
Emanuel Ghetiu <emanuel@optiononetech.com>
2024-10-03 15:41:24 +0300

    Entity created status

---
+++ src/OptionOneTech.AlertSystem.Domain/Statuses/Status.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace OptionOneTech.AlertSystem.Statuses
+{
+    public class Status : FullAuditedEntity<Guid>
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(512)]
+        public string Description { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
