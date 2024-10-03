e81e44beab2ae2e5c7c76eb9a9fb1bcee7109219
Emanuel Ghetiu <emanuel@optiononetech.com>
2024-10-03 17:11:55 +0300

    Create Entity Message

---
+++ src/OptionOneTech.AlertSystem.Domain/Messages/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace OptionOneTech.AlertSystem.Messages
+{
+    public class Message : FullAuditedEntity<Guid>
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string From { get; set; }
+
+        [Required]
+        public Guid SourceId { get; set; }
+
+        [Required]
+        public string SourceType { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+    }
+}
+
