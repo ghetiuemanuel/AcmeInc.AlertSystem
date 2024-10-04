f1b8515ce28520e59497613975daef4a73204193
Emanuel Ghetiu <emanuel@optiononetech.com>
2024-10-04 15:39:17 +0300

    error solved

--- src/OptionOneTech.AlertSystem.Application.Contracts/Lookup/LookupDto.cs
+++ src/OptionOneTech.AlertSystem.Application.Contracts/Lookup/LookupDto.cs
@@ -8,12 +8,11 @@ namespace OptionOneTech.AlertSystem.Lookup
 {
     public class LookupDto<T>
     {
-            public T Id { get; set; }
-            public string Name { get; set; }
-            public override string ToString()
-            {
-                return Name;
-            }
-        }
+       public T Id { get; set; }
+       public string Name { get; set; }
+       public override string ToString()
+       {
+          return Name;
+       }
     }
 }
