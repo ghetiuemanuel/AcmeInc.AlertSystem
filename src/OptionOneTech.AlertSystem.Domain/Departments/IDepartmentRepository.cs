38bc259de2ba280d5d7fe2a5863208145211300f
Emanuel Ghetiu <emanuel@optiononetech.com>
2024-10-04 16:19:18 +0300

    Department lookup modification

--- src/OptionOneTech.AlertSystem.Domain/Departments/IDepartmentRepository.cs
+++ src/OptionOneTech.AlertSystem.Domain/Departments/IDepartmentRepository.cs
@@ -1,8 +1,16 @@
+using OptionOneTech.AlertSystem.Lookup;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Volo.Abp.Domain.Repositories;
 
 namespace OptionOneTech.AlertSystem.Departments;
 
 public interface IDepartmentRepository : IRepository<Department, Guid>
 {
+    public interface IRepository<TEntity, TKey>
+    {
+        Task<List<LookupDto<TKey>>> GetLookupListAsync(long skip, long take);
+    }
+
 }
