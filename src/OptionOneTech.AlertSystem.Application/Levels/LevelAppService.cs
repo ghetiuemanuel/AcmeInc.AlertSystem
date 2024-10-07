2dd7ae2df12cba83711af79e5a44b1b51533c472
Emanuel Ghetiu <emanuel@optiononetech.com>
2024-10-07 11:25:50 +0300

    Levels

--- src/OptionOneTech.AlertSystem.Application/Levels/LevelAppService.cs
+++ src/OptionOneTech.AlertSystem.Application/Levels/LevelAppService.cs
@@ -3,11 +3,16 @@ using OptionOneTech.AlertSystem.Permissions;
 using OptionOneTech.AlertSystem.Levels.Dtos;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
+using OptionOneTech.AlertSystem.Lookup;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
 
 namespace OptionOneTech.AlertSystem.Levels;
 
 
-public class LevelAppService : CrudAppService<Level, LevelDto, Guid, PagedAndSortedResultRequestDto, CreateLevelDto, UpdateLevelDto>,
+public class LevelAppService : CrudAppService<Level, LevelDto, Guid, LevelGetListInput, CreateLevelDto, UpdateLevelDto>,
     ILevelAppService
 {
     protected override string GetPolicyName { get; set; } = AlertSystemPermissions.Level.Default;
@@ -21,6 +26,30 @@ public class LevelAppService : CrudAppService<Level, LevelDto, Guid, PagedAndSor
     public LevelAppService(ILevelRepository repository) : base(repository)
     {
         _repository = repository;
+    }
+    protected override async Task<IQueryable<Level>> CreateFilteredQueryAsync(LevelGetListInput input)
+    {
+        return (await base.CreateFilteredQueryAsync(input))
+            .WhereIf(!input.Name.IsNullOrWhiteSpace(), x => x.Name.Contains(input.Name))
+            .WhereIf(!input.Description.IsNullOrWhiteSpace(), x => x.Description.Contains(input.Description))
+            .WhereIf(input.Active != null, x => x.Active == input.Active);
+    }
+
+    // implement GetLookupAsync from ILookupAppService<Guid>
+    public async Task<PagedResultDto<LookupDto<Guid>>> GetLookupAsync(PagedAndSortedResultRequestDto input)
+    {
+        // get the list of levels using the repository
+        var levels = await _repository.GetLookupListAsync(input.SkipCount, input.MaxResultCount);
+
+        // get total number of levels
+        var totalCount = await _repository.CountAsync(p => p.Active);
+
+        // mapping levels to LookupDto and return PagedResultDto
+        return new PagedResultDto<LookupDto<Guid>>(
+           totalCount,
+           ObjectMapper.Map<List<Level>, List<LookupDto<Guid>>>(levels)
+        );
+
     }
 
 }
