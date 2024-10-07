7081cf466f0f5d3f58c26d58a9d69583feb061e5
Emanuel Ghetiu <emanuel@optiononetech.com>
2024-10-07 13:01:47 +0300

    code allignment

--- src/OptionOneTech.AlertSystem.EntityFrameworkCore/Messages/MessageRepository.cs
+++ src/OptionOneTech.AlertSystem.EntityFrameworkCore/Messages/MessageRepository.cs
@@ -24,7 +24,7 @@ public class MessageRepository : EfCoreRepository<AlertSystemDbContext, Message,
         return await (await GetQueryableAsync())
             .AsNoTracking()
             .Where(message => message.SourceType != null)
-            .Select(message => new Message(message.Id, message.Title, "" ,Guid.Empty,SourceType.Email,""))
+            .Select(message => new Message(message.Id, message.Title, "", Guid.Empty, SourceType.Email, ""))
             .Skip(skip)
             .Take(take)
             .ToListAsync();
