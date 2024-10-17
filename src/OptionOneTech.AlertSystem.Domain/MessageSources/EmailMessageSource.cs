787f462a18bc4ce58bcbf8eacecde43784ff057e
Emanuel Ghetiu <emanuel@optiononetech.com>
2024-10-17 10:21:13 +0300

    EmailMessage

---
+++ src/OptionOneTech.AlertSystem.Domain/MessageSources/EmailMessageSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionOneTech.AlertSystem.MessageSources
+{
+    public class EmailMessageSource
+    {
+        public string Hostname { get; set; }
+        public int Port { get; set; }
+        public bool SSL { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }  
+        public string Folder { get; set; }
+        public bool DeleteAfterDownload { get; set; }
+        public bool Active { get; set; }
+    }
+}
