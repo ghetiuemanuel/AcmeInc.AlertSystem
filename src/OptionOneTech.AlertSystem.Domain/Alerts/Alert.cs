4d732859235157685a7b38407df39e1c2e83db7a
Emanuel Ghetiu <emanuel@optiononetech.com>
2024-10-23 12:25:17 +0300

    Alert entity

---
+++ src/OptionOneTech.AlertSystem.Domain/Alerts/Alert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace OptionOneTech.AlertSystem.Alerts
+{
+    public class Alert : FullAuditedEntity<Guid>
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public Guid MessageId { get; set; }
+        public Guid RuleId { get; set; }
+        public Guid DepartmentId { get; set; }
+        public Guid StatusId { get; set; }
+        public Guid LevelId { get; set; }
+    }
+}
